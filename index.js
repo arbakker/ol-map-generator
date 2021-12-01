bb0135ae16d6a128430593fe999cd75947659449
Anton Bakker <anton.bakker@kadaster.nl>
2021-12-01 23:50:44 +0100

    added color config to featureLayers

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import { default as Map } from 'https://esm.run/ol@6.9.0/src/Map';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'https://esm.run/ol@6.9.0/src/style';
 import 'https://esm.run/mustache@4.2.0/mustache.js';
 import 'https://cdn.jsdelivr.net/npm/ajv@6.12.6/dist/ajv.bundle.js'; // es6 module import only works for 6.X.X, see https://github.com/ajv-validator/ajv/issues/1381#issue-778838527
 import {htmlEncode, getResourceText } from './util.js';
+
+
+
+
+
+
+
+function getGeometryType(input){
+    let geojsonObj = JSON.parse(input)   
+    let geomType = geojsonObj.features[0].geometry.type
+    geomType = geomType.replace("Multi", "")
+    return geomType
+}
+
+
+
 
 // define global map var that is imported by generated code
 var map = {}; 
@@ -52,11 +69,19 @@ function handleRejection (p) {
     }))
 }
 function waitForAll (...ps) {
-    console.log(ps)
     return Promise.all(ps.map(handleRejection))
 }
 
 
+function refreshTitle(title="TEST"){
+    let titleEl = document.getElementById("mapTitle")
+    titleEl.innerText = title
+    if (title!==""){
+        titleEl.style.display= 'block';
+    }else{
+        titleEl.style.display= 'none';
+    }
+}
 
 function refreshMap (htmlTemplate, codeTemplate, schemaObject) {
     document.getElementById('jsonInput').classList.remove('invalid')
@@ -66,6 +91,13 @@ function refreshMap (htmlTemplate, codeTemplate, schemaObject) {
 
     const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
     const validate = ajv.compile(schemaObject)
+
+
+    config.featureLayers.map(x=>{
+        let geomType = getGeometryType(JSON.stringify(x.source))
+        x.geomType = geomType
+    })
+
 
     const valid = validate(
         config
@@ -91,22 +123,67 @@ function refreshMap (htmlTemplate, codeTemplate, schemaObject) {
     map.on('moveend', function (e) {
         updateMapState(map)
     })
-    let featureCollections  = config.featureCollections.filter(x => typeof x !== 'string' )
-    let jsonUrls = config.featureCollections.filter(x => typeof x === 'string' )
-    console.log(jsonUrls)
-
-
+    refreshTitle(Object.keys(config).includes('mapTitle')? config.mapTitle: '')
+    let featureCollections  = config.featureLayers.filter(x => typeof x !== 'string' )
+    let jsonUrls = config.featureLayers.filter(x => typeof x === 'string' )
     let promises = jsonUrls.map(url=> getResourceText(url))
-    console.log(promises)
+
+
+    var wmsTemplate = `new ImageLayer({
+        extent: [-100267.6894, 6337518.8850, 1671848.3744, 7255986.2169],
+        source: new ImageWMS({
+            url: '{{{ serviceUrl }}}',
+            params: { 'LAYERS': '{{ layerName }}' },
+            ratio: 1
+        }),
+        {{ #opacity}}
+        opacity: {{ opacity }},
+        {{ /opacity }}
+        {{ #grayscale}}
+        grayscale: {{ grayscale }},
+        {{ /grayscale }}
+    }),`
+    var wmtsTemplate = `new TileLayer({
+        source: new WMTS({    
+            url: '{{{ serviceUrl }}}',
+            layer: '{{ layerName }}',
+            matrixSet: 'EPSG:3857',
+            format: 'image/png',
+            projection: projection,
+            tileGrid: new WMTSTileGrid({
+                origin: getTopLeft(projectionExtent),
+                resolutions: resolutions,
+                matrixIds: matrixIds,
+            }),
+            style: 'default',
+            wrapX: true,
+        }),
+        {{ #opacity}}
+        opacity: {{ opacity }},
+        {{ /opacity }}
+        {{ #grayscale}}
+        grayscale: {{ grayscale }},
+        {{ /grayscale }}
+    }),`
+
+
     waitForAll(...promises).then(
         results=>{
-            let featureCollectionsString = featureCollections.map(x => JSON.stringify(x))
-            featureCollectionsString = featureCollectionsString.concat(results.map(x=>  JSON.stringify(JSON.parse(x))))
-            config.featureCollectionsString = featureCollectionsString
+            
+
+            config.featureLayers = config.featureLayers.map(x=> {
+                let source = x.source
+                source = JSON.stringify(source)
+                x.source = source
+                return x
+            })
+            config["layer_renderer"] = function(){
+                return Mustache.render('{{> ' + this.serviceType  + '}}', this, {WMTS: wmtsTemplate,WMS: wmsTemplate, });
+            }
             let jsCode = Mustache.render(
                 codeTemplate
                 , config);
-            console.log(jsCode)
+            console.log('jsCode', jsCode)
             scriptEl.text = jsCode
             document.head.appendChild(scriptEl)
             // the generated code imports the mapobject from this main script (index.js)
@@ -123,6 +200,7 @@ function refreshMap (htmlTemplate, codeTemplate, schemaObject) {
             )
             const htmlCodeEncoded = htmlEncode(htmlCode)
             document.getElementById('htmlOutput').innerHTML = htmlCodeEncoded
+
         }
     )
 
@@ -133,7 +211,6 @@ function refreshMap (htmlTemplate, codeTemplate, schemaObject) {
 waitForAll(getResourceText('./templates/index.html.template'), getResourceText('./templates/script.js.template'), getResourceText('./default-config.json'), getResourceText('./schema.json')).
     then(
         results => {
-            console.log(results)
             const htmlTemplate = results[0]
             const codeTemplate = results[1]
             const initConfig = JSON.parse(results[2])
